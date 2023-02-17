626ae69b6a46ea6c5ffe6b3463fcf8709dbf2607
Peter <pschoenbauer@gmail.com>
2023-02-16 22:36:11 -0500

    commit tree 5a989e5e50e6fd115df0291f54a9063a3655ff52

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b){
+return(a+b)
+}
+console.log (add(12,10));
+
+function subtract(a,b){
+return(a-b)
+}
+console.log (subtract(24,2))
+
+function multiply(x,y){
+return (x*y)
+}
+console.log (multiply (2,11))
+
+function divide(q,w){
+return (q/w)
+}
+console.log (divide(66,3))
+
+function increment(n){
+return (n+1)
+}
+console.log (increment(1))
+
+function decrement(n){
+return (n-1)
+}
+console.log (decrement(23))
+
+function makeInt(n){
+return (parseInt (n, 10))
+}
+console.log (makeInt("22.22"))
+
+
+function preserveDecimal(n){
+    return (parseFloat (n, 10))
+    }
+    console.log (preserveDecimal("22.22"))
